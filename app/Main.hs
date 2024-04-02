40b8f443fa3c7f60dc3ca2e6e863d15408f159fc
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-04-02 12:11:24 -0400

    pass stage 4

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,35 @@
 module Main (main) where
 
 import Control.Monad (forever)
+import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy.Char8 as BLC
 import Network.Socket
 import Network.Socket.ByteString (recv, send)
 import System.IO (BufferMode (..), hSetBuffering, stdout)
+
+concatHeader :: (BC.ByteString, BC.ByteString) -> BC.ByteString
+concatHeader (key, value) = key <> ": " <> value
+
+handleRoute :: BC.ByteString -> [BC.ByteString] -> (BC.ByteString, [(BC.ByteString, BC.ByteString)], BC.ByteString)
+handleRoute "GET" [""] = ("200 OK", [], "")
+handleRoute "GET" ("echo" : path) = ("200 OK", [contentType, contentLength], body)
+  where
+    body = BC.intercalate "/" path
+    contentType = ("Content-Type", "text/plain")
+    contentLength = ("Content-Length", BLC.toStrict . BB.toLazyByteString . BB.intDec $ BC.length body)
+handleRoute _ _ = ("404 Not Found", [], "")
+
+handleRequest :: BC.ByteString -> IO BC.ByteString
+handleRequest req = do
+  let requestLines = BC.lines req
+      statusLine = BC.words $ head requestLines
+      method = head statusLine
+      path = tail $ BC.split '/' $ statusLine !! 1
+      version = statusLine !! 2
+      (resStatusCode, resHeaders, resBody) = handleRoute method path
+
+  return $ BC.intercalate "\r\n" [BC.intercalate " " [version, resStatusCode], BC.intercalate "\r\n" (map concatHeader resHeaders), "", resBody]
 
 main :: IO ()
 main = do
@@ -31,10 +56,6 @@ main = do
     (clientSocket, clientAddr) <- accept serverSocket
     BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr)
 
-    req <- recv clientSocket 1024
-    let response = case BC.words req of
-          (_ : "/" : _) -> "HTTP/1.1 200 OK\r\n\r\n"
-          _ -> "HTTP/1.1 404 Not Found\r\n\r\n"
-    _ <- send clientSocket response
+    _ <- recv clientSocket 1024 >>= handleRequest >>= send clientSocket
 
     close clientSocket
