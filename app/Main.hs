b666b3a6ce2193db497ca094331c8f22327fc7be
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-04-02 09:29:03 -0400

    pass 2nd stage

--- app/Main.hs
+++ app/Main.hs
@@ -5,28 +5,31 @@ module Main (main) where
 import Control.Monad (forever)
 import qualified Data.ByteString.Char8 as BC
 import Network.Socket
+import Network.Socket.ByteString (recv, send)
 import System.IO (BufferMode (..), hSetBuffering, stdout)
 
 main :: IO ()
 main = do
-    hSetBuffering stdout LineBuffering
+  hSetBuffering stdout LineBuffering
 
-    let host = "127.0.0.1"
-        port = "4221"
+  let host = "127.0.0.1"
+      port = "4221"
 
-    BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
+  BC.putStrLn $ "Listening on " <> BC.pack host <> ":" <> BC.pack port
 
-    -- Get address information for the given host and port
-    addrInfo <- getAddrInfo Nothing (Just host) (Just port)
+  -- Get address information for the given host and port
+  addrInfo <- getAddrInfo Nothing (Just host) (Just port)
 
-    serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
-    bind serverSocket $ addrAddress $ head addrInfo
-    listen serverSocket 5
+  serverSocket <- socket (addrFamily $ head addrInfo) Stream defaultProtocol
+  bind serverSocket $ addrAddress $ head addrInfo
+  listen serverSocket 5
 
-    -- Accept connections and handle them forever
-    forever $ do
-        (clientSocket, clientAddr) <- accept serverSocket
-        BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-        -- Handle the clientSocket as needed...
+  -- Accept connections and handle them forever
+  forever $ do
+    (clientSocket, clientAddr) <- accept serverSocket
+    BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr)
 
-        close clientSocket
+    _ <- recv clientSocket 1024
+    _ <- send clientSocket $ BC.pack "HTTP/1.1 200 OK\r\n\r\n"
+
+    close clientSocket
